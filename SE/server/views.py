5e50ae8c79f34177400b242b3aa50102112133ab
Amartya Vats <amartyavats@Amartyas-MBP.students.jacobs-university.de>
2021-03-09 08:39:24 +0100

    updated with working buttons

--- SE/server/views.py
+++ SE/server/views.py
@@ -38,19 +38,29 @@ def register(request):
 
 class GameCreationForm(forms.Form):
     game_name = forms.CharField(label = "Game Name")
-    session_length = forms.IntegerField(label = "Length of Game")
+    session_length = forms.IntegerField(label = "Length of Game", min_value=0)
     # players_present = forms.MultipleChoiceField(
     #     queryset = queryset_of_valid_choices, # not optional, use .all() if unsure
     #     widget  = forms.CheckboxSelectMultiple,) #Here we have to 
                             # add the check list with options Distributor and Wholesaler
                             # <input type="checkbox" value="{{item.id}}" name="choices">
+    # max_value=int(session_length/2)
     info_sharing = forms.BooleanField(label="Information sharing allowed")
-    info_delay = forms.IntegerField(label="Information Delay")
-    starting_inventory = forms.IntegerField(label="Inventory Game starts with")
-    holding_cost = forms.IntegerField(label="Holding Cost")
-    backlog_cost = forms.IntegerField(label="Backlog Cost")
+    info_delay = forms.IntegerField(label="Information Delay", min_value=0)
+    starting_inventory = forms.IntegerField(label="Inventory Game starts with", min_value=0)
+    holding_cost = forms.IntegerField(label="Holding Cost", min_value=0)
+    backlog_cost = forms.IntegerField(label="Backlog Cost", min_value=0)
 
 def create(request):
+    if request.method == "POST":
+        form = GameCreationForm(request.POST)
+        if (form.is_valid()):
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, "server/create.html" , {
+                "form" : form
+            })
+
     return render(request, "server/create.html", {
             "form" : GameCreationForm()
     })
