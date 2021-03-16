021638e402140ef1b6d4e10c3883e7f1bee0ec1e
Nodirbek Ibragimov <59182238+nibragimov@users.noreply.github.com>
2021-03-16 01:09:58 +0100

    Delete SE/player directory

--- SE/player/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("logged_in.html", views.logged_in, name="logged_in"),
-    path ("create.html", views.create, name="create")
-]
