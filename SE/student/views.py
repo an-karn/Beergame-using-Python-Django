70ace37e7d6428306fb37aa9f7aa443fd2c229b9
nibragimov <nodib7034@gmail.com>
2021-03-22 18:04:33 +0100

    added add _to_ assigned_games func for Student

--- SE/student/views.py
+++ SE/student/views.py
@@ -17,7 +17,7 @@ def student_registration(request):
 		form = StudentGameRegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return redirect(reverse('enter-game'))
+			messages.success(request, 'You have registered successfully!')
 			
 	context = {'form':form}
 	return render(request, 'student/student-game-registration.html', context)
@@ -33,7 +33,9 @@ def enter_game(request):
 		try:
 			student = Student.objects.get(email=email)
 			game_id = request.POST.get('game_id')
-			
+
+			add_to_assigned_games(student, game_id)
+
 			return redirect(reverse('game-home', args=[game_id]))
 
 		except Student.DoesNotExist:
@@ -42,6 +44,26 @@ def enter_game(request):
 	context = {'form':form}
 	return render(request, 'student/student-enter-game.html', context)
 
+
+#add to assigned_games field in Student model
+def add_to_assigned_games(student, game_id):
+	assigned_games = []
+	#add game_id to student assigned games 
+	if student.assigned_games == 'None':
+		student.assigned_games = ''
+		student.assigned_games += game_id 
+	else:
+		assigned_games = student.assigned_games.split(',')
+		ignore = False
+		#make sure that we have no duplicate game_ids in Student model
+		for game in assigned_games:
+			if game == game_id:
+				ignore = True
+				break
+		if not ignore:
+			student.assigned_games += ',' + game_id
+			
+	student.save()
 
 def play_game(request, pk):
 	return render(request, 'student/student-home.html')
